70c9844e55aa8a719a01a2a64fea4472a0cb4cac
SiterBoy <phpdevelop@ya.ru>
2022-11-21 00:05:04 +0600

    fix dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response} from "express";
 import bodyParser from "body-parser";
+import * as dotenv from 'dotenv';
+dotenv.config()
 import {videosRouter} from "./routes/videos-router";
 import {videos} from "./routes/videos-router";
 const port = process.env.PORT || 30012;
 export const app = express();
-
 app.use(bodyParser.json());
 
 app.delete('/testing/alldata', (req: Request, res:Response) => {
