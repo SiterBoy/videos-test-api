49dcf3ce199ff8b19a628eea4b4b8d67c50ada05
SiterBoy <phpdevelop@ya.ru>
2022-11-21 14:06:23 +0600

    title check type field

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -9,20 +9,13 @@ export const validateVideo = (data:IVideoPostPut): ErrorsType   => {
   let { title, author, canBeDownloaded, minAgeRestriction,  availableResolutions, publicationDate} = data;
 
   if(!author) {
-    errors.push({message: 'Author required', field: 'Author'})
+    errors.push({message: 'Author required', field: 'author'})
   }
 
   if(!title) {
-    errors.push({message: 'Title required', field: 'Title'})
+    errors.push({message: 'Title required', field: 'title'})
   }
 
-  if(typeof title !== 'string') {
-    errors.push({message: 'Title should be a string', field: "title"})
-  }
-
-  if(typeof author !== 'string') {
-    errors.push({message: 'Author should be a string', field: 'author'})
-  }
 
   if(availableResolutions && !Array.isArray(availableResolutions)) {
     errors.push({message: 'availableResolutions should be Array', field: 'availableResolutions'})
@@ -37,11 +30,11 @@ export const validateVideo = (data:IVideoPostPut): ErrorsType   => {
   }
 
   if(title?.length > 40) {
-    errors.push({message: 'Title length must be shorter 40 symbols', field: 'Title'})
+    errors.push({message: 'Title length must be shorter 40 symbols', field: 'title'})
   }
 
   if(author?.length > 20) {
-    errors.push({message: 'Author length must be shorter 20 symbols', field: 'Author'})
+    errors.push({message: 'Author length must be shorter 20 symbols', field: 'author'})
   }
 
   if(minAgeRestriction && (minAgeRestriction < 1 || minAgeRestriction > 18)) {
